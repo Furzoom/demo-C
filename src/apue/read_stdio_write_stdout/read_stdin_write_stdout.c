0c5078e7a07b9d9a0243d92a62ec5d909d2ac079
furzoom <furzoommn@gmail.com>
2016-03-15 11:22:05 +0800

    STDIN_FILENO and STDOUT_FILENO, STDERR_FILENO

---
+++ src/apue/read_stdio_write_stdout/read_stdin_write_stdout.c
@@ -0,0 +1,20 @@
+/*
+ * read from stdin and write to stdout
+ */
+#include "unp.h"
+
+int main()
+{
+    int n;
+    char buf[BUFFSIZE];
+    
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+    {
+        if (write(STDOUT_FILENO, buf, n) != n)
+            err_sys("write error");
+    }
+    if (n < 0)
+        err_sys("read error");
+    return 0;
+}
+
