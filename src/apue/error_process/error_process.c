29e8b4c20c41d9f84ff2da25a5ed0bfa33eaf970
furzoom <furzoommn@gmail.com>
2016-03-15 15:18:17 +0800

    error process

---
+++ src/apue/error_process/error_process.c
@@ -0,0 +1,14 @@
+/*
+ * perror() and strerror()
+ */
+
+#include "unp.h"
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    fprintf(stderr, "EACCES: %s\n", strerror(EACCES));
+    errno = ENOENT;
+    perror(argv[0]);
+    return 0;
+}
