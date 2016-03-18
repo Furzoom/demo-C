457be523b5b5bc867305653b861fb77481aee4a6
furzoom <furzoommn@gmail.com>
2016-03-18 16:19:01 +0800

    link

---
+++ src/apue/link/link.c
@@ -0,0 +1,18 @@
+/*
+ * link()
+ */
+
+#include "unp.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+        err_quit("usage: %s <oldpath> <newpath>", argv[1]);
+    if (access(argv[1], F_OK) == -1)
+        err_sys(argv[1]);
+    if (access(argv[2], F_OK) == 0)
+        err_msg("%s is existed", argv[2]);
+    if (link(argv[1], argv[2]) == -1)
+        err_msg("link error");
+    return 0;
+}
