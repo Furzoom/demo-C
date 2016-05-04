34eb4a0e42a0e07a159430c8e12efe26d15d7cc5
furzoom <furzoommn@gmail.com>
2016-05-04 09:58:11 +0800

    modify test.cpp

--- src/cpp/essential_cpp/test.cpp
+++ src/cpp/essential_cpp/test.cpp
@@ -6,6 +6,8 @@ class Foo
 {
     public:
         Foo() { cout << "Foo()\n"; }
+        Foo(const Foo& f) { cout << "Foo(const Foo& f)\n"; }
+        Foo(Foo& f) { cout << "Foo(Foo& f)\n"; }
         ~Foo() { cout << "~Foo()\n"; }
 };
 
@@ -14,10 +16,18 @@ void test(Foo f)
     cout << "test()\n";
 }
 
+void test1(const Foo f)
+{
+    cout << "test1()\n";
+}
+
 int main()
 {
-
-    test(Foo());
+    Foo f;
+    test(f);
+    cout << "test over\n";
+    const Foo f1;
+    test1(f1);
     cout << "test over\n";
     return 0;
 }
