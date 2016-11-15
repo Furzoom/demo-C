efa33b844f2f81293a3ff0bf53dfa09db6815bb5
furzoom <furzoommn@gmail.com>
2016-11-15 15:44:38 +0800

    add some other file function

--- src/apue/read_stdin_write_stdout2/read_stdin_write_stdout2.c
+++ src/apue/read_stdin_write_stdout2/read_stdin_write_stdout2.c
@@ -11,6 +11,11 @@ int main()
             err_sys("stdout error");
     if (ferror(stdin))
         err_sys("stdin error");
+    if (feof(stdin))
+        printf("EOF stdin\n");
+    printf("stdin fd %u\n", fileno(stdin));
+    printf("stdout fd %u\n", fileno(stdout));
+    printf("stderr fd %u\n", fileno(stderr));
 
     return 0;
 }
