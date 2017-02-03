0f74366de1cd57f9904fd7b0389b61e06afc4f21
furzoom <furzoommn@gmail.com>
2017-02-03 16:21:26 +0800

    add get UID EUID GID EGID

--- src/apue/getpid/getpid.c
+++ src/apue/getpid/getpid.c
@@ -7,5 +7,10 @@ int main()
 {
     printf("PID : %d\n", getpid());
     printf("PPID: %d\n", getppid());
+    printf("UID:  %d\n", getuid());
+    printf("EUID: %d\n", geteuid());
+    printf("GID:  %d\n", getgid());
+    printf("EGID: %d\n", getegid());
+
     return 0;
 }
