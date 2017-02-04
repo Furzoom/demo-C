689d826a8ca2c8523cbe59d67e04813bea6f6ba9
furzoom <furzoommn@gmail.com>
2017-02-04 10:01:50 +0800

    extract pr_exit function to unp.h

--- src/apue/system/system.c
+++ src/apue/system/system.c
@@ -1,7 +1,6 @@
 #include "unp.h"
 #include <sys/wait.h>
 
-void pr_exit(int status);
 
 int main(int argc, char *argv[])
 {
@@ -21,20 +20,3 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void pr_exit(int status)
-{
-    if (WIFEXITED(status))
-        printf("normal termination, exit status = %d\n",
-                WEXITSTATUS(status));
-    else if (WIFSIGNALED(status))
-        printf("abnormal termination, signal number = %d%s\n",
-                WTERMSIG(status),
-#ifdef WCOREDUMP
-                WCOREDUMP(status) ? " (core file generated)" : "");
-#else
-                "");
-#endif
-    else if (WIFSTOPPED(status))
-        printf("child stopped, signal number = %d\n",
-                WSTOPSIG(status));
-}
