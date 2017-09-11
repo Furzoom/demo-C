a8b6b6df04549c6fa3f57a242b759f9d0b2c3114
furzoom <mn@furzoom.com>
2017-09-11 23:54:06 +0800

    fixed

--- src/apue/access_file/access_file.c
+++ src/apue/access_file/access_file.c
@@ -9,6 +9,9 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
         err_quit("usage: %s <pathname>", argv[0]);
+    if (access(argv[1], F_OK) < 0)
+        err_quit("file %s does not exist", argv[1]);
+
     if (access(argv[1], R_OK) < 0)
         err_ret("access error for %s", argv[1]);
     else
